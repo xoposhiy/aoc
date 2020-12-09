255b24711b3dd07711ff33a691591554baacfa27
Pavel Egorov <xoposhiy@mail.ru>
2020-12-09 13:08:23 +0500

    Day 9

--- 09.cs
+++ 09.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.IO;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,9 +7,93 @@ public class Day9
 {
     public void Solve()
     {
-        File.ReadAllLines("09.txt");
+        var numbers = File.ReadLines("09.txt").Select(long.Parse).ToList();
+        
+        var invalidNumber = GetInvalidNumber(numbers, 25);
+        Console.WriteLine($"Part One: {invalidNumber}");
+        
+        var seq = GetSliceWithSum(numbers, invalidNumber);
+        Console.WriteLine($"Part Two: {seq.Min() + seq.Max()}");
+    }
 
-        Console.WriteLine($"Part One: {1}");
-        Console.WriteLine($"Part Two: {2}");
+    private static long GetInvalidNumber(List<long> numbers, int size)
+    {
+        //                  window
+        // numbers: 1 2 3 [4 5 6 7 8] 9 10 ...
+        
+        var window = new Queue<long>();
+        foreach (var n in numbers)
+        {
+            if (window.Count == size)
+            {
+                if (!IsSum(n, window)) 
+                    return n;
+                window.Dequeue();
+            }
+            window.Enqueue(n);
+        }
+        throw new Exception("no weakness");
+    }
+
+    private static bool IsSum_Naïve(long targetSum, IReadOnlyCollection<long> window)
+    {
+        // 25*25 = 625
+        foreach (var a in window)
+            foreach (var b in window)
+                if (a + b == targetSum)
+                    return true;
+        return false;
+    }
+
+    private static bool IsSum_Better(long targetSum, IReadOnlyCollection<long> window)
+    {
+        var set = window.ToHashSet();
+        foreach (var a in window)
+            if (set.Contains(targetSum - a))
+                return true;
+        return false;
+    }
+
+    private static bool IsSum(long targetSum, IReadOnlyCollection<long> window)
+    {
+        var set = window.ToHashSet();
+        return window.Any(a => a != targetSum - a && set.Contains(targetSum - a)); // window.some(...)
+    }
+
+
+    public IReadOnlyCollection<long> GetSliceWithSum_Naïve(List<long> numbers, in long targetSum)
+    {
+        // targetSum = 8
+        // [] 1 2 3 4 3 1 10
+
+        var slice = new Queue<long>();
+        foreach (var num in numbers)
+        {
+            slice.Enqueue(num);
+            while (slice.Sum() > targetSum)
+                slice.Dequeue();
+            if (slice.Sum() == targetSum && slice.Count > 1)
+                return slice;
+        }
+        throw new Exception("no weakness");
+    }
+
+    public IReadOnlyCollection<long> GetSliceWithSum(List<long> numbers, in long targetSum)
+    {
+        // targetSum = 8
+        // [] 1 2 3 4 3 1 10
+        
+        var slice = new Queue<long>();
+        long sliceSum = 0;
+        foreach (var num in numbers)
+        {
+            sliceSum += num;
+            slice.Enqueue(num);
+            while (sliceSum > targetSum)
+                sliceSum -= slice.Dequeue();
+            if (sliceSum == targetSum && slice.Count > 1)
+                return slice;
+        }
+        throw new Exception("no weakness");
     }
 }
--- main.cs
+++ main.cs
@@ -4,10 +4,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text.RegularExpressions;
 
-new Day8().Solve();
+//new Day8().Solve();
 new Day9().Solve();
 
+/*
 T[] ParseMany<T>(string input, string pattern, Func<string, string, T> parse){
     var res = Regex.Matches(input, pattern);
     return res.Select(m => parse(m.Groups[1].Value, m.Groups[2].Value)).ToArray();
 }
+
+*/
