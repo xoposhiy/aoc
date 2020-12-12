7fb3def96d18f02bf21de0882f9baac5f2377a44
Pavel Egorov <xoposhiy@mail.ru>
2020-12-12 11:24:48 +0500

    Day 12 refactoring

--- 12_ferry.cs
+++ 12_ferry.cs
@@ -1,79 +1,94 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.IO;
 using System.Linq;
-using static System.Linq.Enumerable;
 using static System.Math;
 
 public class Day12
 {
-    public record Command(char dir, int count);
-    public record Ferry(char dir, int x, int y);
-    public record Point(int x, int y);
+    public record Command(char Dir, int Count);
+    public record Ferry(char Dir, Point Pos);
+
+    public record Point(int X, int Y)
+    {
+        public static Point operator +(Point a, Point b) => new(a.X + b.X, a.Y + b.Y);
+        public static Point operator -(Point a, Point b) => new(a.X - b.X, a.Y - b.Y);
+        public static Point operator *(Point a, int b) => new(a.X * b, a.Y * b);
+        public int MLen => Abs(X) + Abs(Y);
+    }
 
     public void Solve()
     {
-        var ns = File.ReadLines("12.txt")
-            .Select(s => new Command(s[0], int.Parse(s.Substring(1))))
+        var commands = File.ReadLines("12.txt")
+            .Select(s => new Command(s[0], int.Parse(s[1..])))
             .ToArray();
-        var ferry = new Ferry('E', x: 0, y: 0);
-        ferry = ns.Aggregate(ferry, Move);
+        Part1(commands);
+        Part2(commands);
+    }
 
-        Console.WriteLine($"Part One: {Abs(ferry.x) + Abs(ferry.y)}");
-        var wp = new Point(x: 10, y: -1);
-        var pos = new Point(x: 0, y: 0);
-        foreach (var step in ns)
+    private void Part1(Command[] commands)
+    {
+        var ferry = new Ferry('E', new(0, 0));
+        ferry = commands.Aggregate(ferry, Move);
+        Console.WriteLine($"Part One: {ferry.Pos.MLen}");
+    }
+
+    private void Part2(Command[] commands)
+    {
+        var wp = new Point(10, -1);
+        var pos = new Point(0, 0);
+        foreach (var command in commands)
         {
-            if (step.dir == 'F')
-                pos = new Point(pos.x + wp.x * step.count, pos.y + wp.y * step.count);
+            if (command is ('F', var count))
+                pos += wp * count;
             else
-                wp = MoveWayPoint(wp, step);
+                wp = MoveWayPoint(wp, command);
         }
-        Console.WriteLine($"Part Two: {Abs(pos.x) + Abs(pos.y)}");
+
+        Console.WriteLine($"Part Two: {pos.MLen}");
     }
 
     private Point MoveWayPoint(Point wp, Command cmd)
     {
         return cmd switch
         {
-            ('R', var len) => RotateWpRight(wp, (len / 90) % 4),
-            ('L', var len) => RotateWpRight(wp, 4-(len / 90) % 4),
-            var (dir, len) => new Point(wp.x + deltas[dir].x * len, wp.y + deltas[dir].y * len)
+            ('R', var len) => RotateWayPointRight(wp, (len / 90) % 4),
+            ('L', var len) => RotateWayPointRight(wp, 4 - (len / 90) % 4),
+            var (dir, len) => wp + deltas[dir] * len
         };
     }
 
-    private Point RotateWpRight(Point wp, int len)
+    private Point RotateWayPointRight(Point wp, int len)
     {
         for (int i = 0; i < len; i++)
-            wp = new Point(-wp.y, wp.x);
+            wp = new Point(-wp.Y, wp.X);
         return wp;
     }
 
     private static readonly Dictionary<char, char> nextDir = new()
     {
-        {'E', 'S'},
-        {'S', 'W'},
-        {'W', 'N'},
-        {'N', 'E'},
+        { 'E', 'S' },
+        { 'S', 'W' },
+        { 'W', 'N' },
+        { 'N', 'E' },
     };
 
-    private static readonly Dictionary<char, (int x, int y)> deltas= new()
+    private static readonly Dictionary<char, Point> deltas = new()
     {
-        {'E', (1, 0)},
-        {'S', (0, 1)},
-        {'W', (-1, 0)},
-        {'N', (0, -1)},
+        { 'E', new(1, 0) },
+        { 'S', new(0, 1) },
+        { 'W', new(-1, 0) },
+        { 'N', new(0, -1) },
     };
 
     private Ferry Move(Ferry ferry, Command cmd)
     {
         return cmd switch
         {
-            ('R', var len) => ferry with { dir = RotateRight(ferry.dir, (len / 90) % 4) },
-            ('L', var len) => ferry with { dir = RotateRight(ferry.dir, 4 - (len / 90) % 4) },
-            ('F', var len) => ferry with { x = ferry.x + deltas[ferry.dir].x * len, y = ferry.y + deltas[ferry.dir].y * len },
-            var (dir, len) => new Ferry(ferry.dir, ferry.x + deltas[dir].x * len, ferry.y + deltas[dir].y * len)
+            ('R', var len) => ferry with { Dir = RotateRight(ferry.Dir, (len / 90) % 4) },
+            ('L', var len) => ferry with { Dir = RotateRight(ferry.Dir, 4 - (len / 90) % 4) },
+            ('F', var len) => ferry with { Pos = ferry.Pos + deltas[ferry.Dir] * len },
+            var (dir, len) => ferry with { Pos = ferry.Pos + deltas[dir] * len }
         };
     }
 
