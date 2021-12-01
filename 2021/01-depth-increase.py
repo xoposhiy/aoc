15cef59a1d717d26914f5f211aed1e3e5e273b82
pe <xoposhiy@mail.ru>
2021-12-02 01:51:05 +0500

    DAY1

--- 2021/01-depth-increase.py
+++ 2021/01-depth-increase.py
@@ -1,8 +1,14 @@
 import aoc
+import more_itertools
 
-ds = aoc.read_ints()
-c1 = sum(1 for t in zip(ds, ds[1:]) if t[0] < t[1])
+
+input = aoc.read_ints()
+c1 = sum(a < b for a, b in zip(input, input[1:]))
 print("Part One", c1)
 
-c2 = sum(1 for t in zip(ds, ds[3:]) if t[0] < t[1])
-print("Part Two", c2)
+c2 = sum(a < b for a, b in zip(input, input[3:]))
+c22 = sum(sum(input[i:i + 3]) < sum(input[i + 1:i + 4]) for i in range(0, len(input) - 3))
+c23 = sum(w[0] < w[-1] for w in more_itertools.windowed(input, 4))
+c24 = len([1 for a, b in zip(input, input[3:]) if a < b])
+
+print("Part Two", c2, c22, c23, c24)
