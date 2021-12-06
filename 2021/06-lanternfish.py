dd947f9425fe8004a3c683c5b17e61f3141909c0
pe <xoposhiy@mail.ru>
2021-12-07 00:39:46 +0500

    DAY06

--- 2021/06-lanternfish.py
+++ 2021/06-lanternfish.py
@@ -1,3 +1,5 @@
+import functools
+
 from aoc import *
 import numpy as np
 
@@ -19,6 +21,10 @@ def numpy_solve(cooldowns, days):
     return counts.sum()
 
 
+def oneliner(cooldowns, days):
+    return functools.reduce(lambda counts, _: np.roll(counts, -1) + counts[0] * np.eye(9, dtype=int)[6], range(days), np.unique(cooldowns + list(range(0, 9)), return_counts=True)[1] - [1] * 9).sum()
+
+
 def smart_solve(cooldowns, days):
     counts = [0] * 9
     for c in cooldowns:
@@ -37,9 +43,24 @@ def matrix_solve(cooldowns, days):
     return np.dot(np.linalg.matrix_power(matrix, days), np.array(counts)).sum()
 
 
+magics = {
+    80: [1401, 1191, 1154, 1034, 950],
+    256: [6206821033, 5617089148, 5217223242, 4726100874, 4368232009]
+}
+
+
+def precalc_solve(cooldowns, days):
+    return sum(cooldowns.count(cooldown) * magic for cooldown, magic in enumerate(magics[days], 1))
+
+
 inp = read(sep=",")[0]
 print("Part One", solve(inp, 80))
 print("Part Two", solve(inp, 256))
 print("Part Two (numpy)", numpy_solve(inp, 256))
 print("Part Two (smart)", smart_solve(inp, 256))
 print("Part Two (matrix)", matrix_solve(inp, 256))
+print("Part Two (oneliner)", oneliner(inp, 256))
+print("Part Two (precalc)", precalc_solve(inp, 256))
+
+
+
