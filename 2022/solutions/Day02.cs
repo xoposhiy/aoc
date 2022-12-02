964e986f97f41174bf59ae9362a708bd781d63aa
Pavel Egorov <xoposhiy@mail.ru>
2022-12-02 11:13:09 +0400

    refactor

--- 2022/solutions/Day02.cs
+++ 2022/solutions/Day02.cs
@@ -2,14 +2,14 @@
 {
     public void Solve(string[] lines)
     {
+        int Score(int opp, int me) =>
+            me + ((me - opp + 4) % 3) * 3 + 1;
+
+        int Score2(int opp, int outcome) =>
+            Score(opp, (opp + outcome - 1 + 3) % 3);
+
         var rounds = lines.ParseLines<char[]>().Select(p => (p[0]-'A', p[1]-'X')).ToList();
         Console.WriteLine("Part1: " + rounds.Sum(r => Score(r.Item1, r.Item2)));
         Console.WriteLine("Part2: " + rounds.Sum(r => Score2(r.Item1, r.Item2)));
     }
-
-    private int Score(int opp, int me) => 
-        me + ((me - opp + 4) % 3)*3 + 1;
-
-    private int Score2(int opp, int outcome) => 
-        Score(opp, (opp + outcome - 1 + 3) % 3);
 }
