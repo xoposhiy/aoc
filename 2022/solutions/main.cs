5ac4345784368ab6b951a9451479209073f2760f
Pavel Egorov <xoposhiy@mail.ru>
2022-12-08 16:35:02 +0400

    Refactoring

--- 2022/solutions/main.cs
+++ 2022/solutions/main.cs
@@ -3,40 +3,46 @@ using System.Net;
 using System.Reflection;
 
 Console.WriteLine("# Advent of Code 2022");
+
 Console.WriteLine();
 
 await SolveDay(DateTime.Now.Day);
+
 //foreach (var dayNumber in Enumerable.Range(1, DateTime.Now.Day)) await SolveDay(dayNumber);
-
-async Task SolveDay(int n)
+async Task SolveDay(int day)
 {
-    Console.WriteLine("## Day " + n);
+    Console.WriteLine("## Day " + day);
 
-    var inputFilename = $"../../../inputs/{n:D2}.txt";
-    if (!File.Exists(inputFilename))
+    var inputFilename = $"../../../inputs/{day:D2}.txt";
+    await DownloadInputIfNeeded(inputFilename, day);
+
+    var daySolution = CreateInstanceForDay(day);
+    var sw = Stopwatch.StartNew();
+    daySolution.GetType().GetMethod("Solve")!.InvokeWithParsedArgs(daySolution, inputFilename);
+    Console.WriteLine("Total time: " + sw.Elapsed);
+    Console.WriteLine();
+}
+
+async Task DownloadInputIfNeeded(string s, int i)
+{
+    if (!File.Exists(s))
     {
-        var inputUrl = $"https://adventofcode.com/2022/day/{n}/input";
+        var inputUrl = $"https://adventofcode.com/2022/day/{i}/input";
         var httpMessageHandler = new HttpClientHandler();
         var aocSession = Environment.GetEnvironmentVariable("AOC");
         if (string.IsNullOrEmpty(aocSession))
-        {
-            Console.WriteLine("AOC environment variable not set");
-            return;
-        }
+            throw new Exception("AOC environment variable not set");
 
         httpMessageHandler.CookieContainer.Add(new Cookie("session", aocSession, "/", "adventofcode.com"));
         var inp = await new HttpClient(httpMessageHandler).GetByteArrayAsync(inputUrl);
-        File.WriteAllBytes(inputFilename, inp);
-        Console.WriteLine($"Input downloaded to {Path.GetFullPath(inputFilename)}");
+        File.WriteAllBytes(s, inp);
+        Console.WriteLine($"Input downloaded to {Path.GetFullPath(s)}");
     }
+}
 
+object CreateInstanceForDay(int dayNumber)
+{
     var assembly = Assembly.GetExecutingAssembly();
-    var dayType = assembly.GetType("Day" + n) ?? assembly.GetType("Day0" + n)!;
-    object res = Activator.CreateInstance(dayType) ?? throw new Exception("oops");
-    var m = dayType.GetMethod("Solve")!;
-
-    var sw = Stopwatch.StartNew();
-    dayType.GetMethod("Solve")!.InvokeWithParsedArgs(res, inputFilename);
-    Console.WriteLine("Total time: " + sw.Elapsed);
-    Console.WriteLine();
+    var dayType = assembly.GetType("Day" + dayNumber) ?? assembly.GetType("Day0" + dayNumber)!;
+    return Activator.CreateInstance(dayType) ?? throw new Exception("oops");
 }
