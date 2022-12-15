77817227e5068bf394db0587ba235a97b49e219b
Pavel Egorov <xoposhiy@mail.ru>
2022-12-15 18:57:05 +0400

    Refactoring

--- 2022/solutions/Day15.cs
+++ 2022/solutions/Day15.cs
@@ -1,8 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-
 [Template(@"Sensor at @Sensor: closest beacon is at @Beacon")]
 public record SensorData([Separator("yx=, ")]V Sensor, [Separator("yx=, ")] V Beacon)
 {
@@ -10,76 +5,52 @@ public record SensorData([Separator("yx=, ")]V Sensor, [Separator("yx=, ")] V Be
 };
 public class Day15
 {
-    IEnumerable<R> GetCoverageSegments(SensorData[] sensors, int y)
-    {
-        return sensors
-            .Select(s => (x: s.Sensor.X, r: s.Distance - Math.Abs(s.Sensor.Y - y)))
-            .Select(xr => new R(xr.x - xr.r, xr.x + xr.r));
-
-    }
-
     public void Solve(SensorData[] sensors)
     {
-        var left = Math.Min(sensors.Min(x => x.Beacon.X), sensors.Min(x => x.Sensor.X));
-        var right = Math.Max(sensors.Max(x => x.Beacon.X), sensors.Max(x => x.Sensor.X));
         var beacons = sensors.Select(x => x.Beacon).ToHashSet();
-        var margin = sensors.Max(x => x.Sensor.MDistTo(x.Beacon)) + 1;
-        margin.Out("Margin: ");
-        var impossiblePositionsCount = 0;
-
-        var y = 2000000;
-        var rs = GetCoverageSegments(sensors, 2000000).ToList();
-        for (int x = left-margin; x <= right+margin;)
-        {
-            var overlappedRs = rs.Where(r => r.Contains(x)).ToList();
-            if (overlappedRs.Any())
-            {
-                var lastX = overlappedRs.Max(t => t.End);
-                impossiblePositionsCount += lastX + 1 - x;
-                x = lastX + 1;
-            }
-            else
-            {
-                var nextRs = rs.Where(r => r.Start > x).ToList();
-                if (nextRs.Any())
-                {
-                    x = nextRs.Min(t => t.Start);
-                }
-                else
-                    break;
-            }
-        }
-
+        var y = 2_000_000;
+        var rs = GetCoverageSegments(sensors, y).Out();
+        var impossiblePositionsCount = rs.Sum(r => r.Len);
         var beaconsCount = beacons.Count(b => b.Y == y);
-        
         (impossiblePositionsCount-beaconsCount).Out("Part 1: ");
 
-
-        var pos = GetUnknownBeaconPosition(sensors).Out("Beacon: ");
+        var pos = GetUnknownBeaconPositions(sensors).Single().Out("Beacon: ");
         long res = pos.X * 4000000L + pos.Y;
         res.Out("Part 2: ");
     }
 
-    private V GetUnknownBeaconPosition(SensorData[] sensors)
+    public List<R> GetCoverageSegments(SensorData[] sensors, int y)
     {
-        for (int y = 0; y <= 4000000; y++)
-        {
-            var rs = GetCoverageSegments(sensors, y).ToList();
-            for (int x = 0; x <= 4000000;)
+        var ranges = sensors
+            .Select(s => (x: s.Sensor.X, r: s.Distance - Math.Abs(s.Sensor.Y - y)))
+            .Where(s => s.r >= 0)
+            .Select(xr => new R(xr.x - xr.r, xr.x + xr.r));
+        var mergedSegments = ranges
+            .OrderBy(r => r.Start)
+            .Aggregate(new List<R>(), (acc, r) =>
             {
-                var overlappedRs = rs.Where(r => r.Contains(x)).ToList();
-                if (overlappedRs.Any())
-                {
-                    var lastX = overlappedRs.Max(t => t.End);
-                    x = lastX + 1;
-                }
+                if (acc.Count == 0 || !acc.Last().Overlaps(r))
+                    acc.Add(r);
                 else
-                {
-                    return new V(x, y);
-                }
-            }
-        }
+                    acc[^1] = acc[^1] with { End = Math.Max(acc[^1].End, r.End) };
+                return acc;
+            });
+        return mergedSegments;
+    }
 
-        throw new Exception("No beacon");
+    public IEnumerable<V> GetUnknownBeaconPositions(SensorData[] sensors)
+    {
+        var windowRange = new R(0, 4_000_000);
+        for (int y = 0; y <= 4_000_000; y++)
+        {
+            var rs = GetCoverageSegments(sensors, y).Where(r => r.Overlaps(windowRange)).ToList();
+            for (int x = 0; x < rs[0].Start; x++)
+                yield return new(x, y);
+            for (int i = 0; i < rs.Count - 1; i++)
+            for (int x = rs[i].End+1; x < rs[i+1].Start; x++)
+                yield return new(x, y);
+            for (int x = rs.Last().End+1; x <= windowRange.End; x++)
+                yield return new(x, y);
+        }
     }
 }
--- 2022/solutions/R.cs
+++ 2022/solutions/R.cs
@@ -3,4 +3,5 @@
     public bool Contains(R other) => Start <= other.Start && End >= other.End;
     public bool Contains(int p) => Start <= p && End >= p;
     public bool Overlaps(R other) => Math.Max(Start, other.Start) <= Math.Min(End, other.End);
+    public int Len => End - Start + 1;
 }
