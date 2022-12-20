93616ea6f73c3c9a07592f9fb226892a7602485e
Pavel Egorov <xoposhiy@mail.ru>
2022-12-21 00:06:39 +0400

    Far links optimization

--- 2022/solutions/Day20.cs
+++ 2022/solutions/Day20.cs
@@ -1,4 +1,5 @@
-﻿using Shouldly;
+﻿using System.Diagnostics;
+using Shouldly;
 
 // ## Grove Positioning System
 public class Day20
@@ -18,10 +19,16 @@ public class Day20
             .Out("Part 2: ").ShouldBe(8927480683);
     }
 
+
     private static long Decrypt(long[] ns, int times)
     {
-        var next = Enumerable.Range(0, ns.Length).Skip(1).Append(0).ToArray();
-        var prev = Enumerable.Range(0, ns.Length - 1).Prepend(ns.Length - 1).ToArray();
+        var n = ns.Length;
+        var next = Enumerable.Range(0, n).Skip(1).Append(0).ToArray();
+        var prev = Enumerable.Range(0, n - 1).Prepend(n - 1).ToArray();
+
+        var farStep = 25;
+        var farNext = Enumerable.Range(0, n).Select(i => i % farStep == 0 ? (i + farStep) % n : -1).ToArray();
+        var farNextLen = Enumerable.Repeat(farStep, n).Skip(1).Append(0).ToArray();
 
         void MoveItem(int x, int shift)
         {
@@ -31,12 +38,67 @@ public class Day20
             var b = next[x];
             next[a] = b;
             prev[b] = a;
+
+            // find closest prev far link
+            var p = a;
+            while (farNext[p] == -1)
+                p = prev[p];
+            // reduce its length because x is gone!
+            farNextLen[p]--;
+            // Oops, what if far link leads exactly to x?
+            if (farNext[p] == x)
+            {
+                // redirect it to a instead. And next far link from x replace with same far link from a:
+                farNext[p] = a;
+                farNext[a] = farNext[x];
+                farNextLen[a] = farNextLen[x];
+                farNext[x] = -1;
+            }
+
+            // Fast search of new x position. Follow far links if can
+            var len = shift;
+            while (len > 0)
+            {
+                if (farNext[a] != -1 && farNextLen[a] <= len && farNextLen[a] > 1)
+                {
+                    len -= farNextLen[a];
+                    a = farNext[a];
+                }
+                else
+                {
+                    len--;
+                    a = next[a];
+                }
+            }
+            b = next[a];
             
+            // Insert x after a
+            next[a] = x;
+            prev[x] = a;
+            next[x] = b;
+            prev[b] = x;
+
+            // Increase len of rev far link, because x is inserted.
+            p = a;
+            while (farNext[p] == -1)
+                p = prev[p];
+            farNextLen[p]++;
+        }
+
+        void MoveItemNoOptimization(int x, int shift)
+        {
+            // a → x → b
+            // Remove x
+            var a = prev[x];
+            var b = next[x];
+            next[a] = b;
+            prev[b] = a;
+
             //Find new a and b
             for (int i = 0; i < shift; i++)
                 a = next[a];
             b = next[a];
-            
+
             // Insert x between new a and b
             next[a] = x;
             prev[x] = a;
@@ -44,11 +106,13 @@ public class Day20
             prev[b] = x;
         }
 
+
         for (int i = 0; i < times; i++)
         {
             var id = i % ns.Length;
             var period = ns.Length - 1;
             var shift = (int)((ns[id] % period + period) % period);
+            //MoveItemNoOptimization(id, shift);
             MoveItem(id, shift);
         }
         var decryptedRing = GetRingContentFromZeroValue(ns, next);
