db07d436329bc97d9b230bd40c4d578f5fdb733f
Pavel Egorov <xoposhiy@mail.ru>
2022-12-21 23:10:18 +0400

    Day 21 Clever solution

--- 2022/solutions/Day21.cs
+++ 2022/solutions/Day21.cs
@@ -17,19 +17,26 @@ public class Day21
         ComputeValue(root.Name, monkeys)
             .Out("Part 1: ").ShouldBe(291425799367130);
 
-
         // Part1
         // Обезьяна root теперь сравнивает два своих аргумента на равенство.
         // А обезьяна humn − это вы. Какое значение должно быть у вас, чтобы у root получилось равенство?
-        monkeys["root"] = root with { Op = "-" };
-        // Binary search correct answer
 
+        // humanFactor * humnValue + free
+        var (humanFactorA, freeA) = GetHumanExpression(root.A, monkeys).Out("human expression for root.A: ");
+        var (humanFactorB, freeB) = GetHumanExpression(root.B!, monkeys).Out("human expression for root.B: ");
+
+        var humanValue1 = (freeB - freeA) / (humanFactorA - humanFactorB);
+        humanValue1.Denominator.ShouldBe(1);
+        humanValue1.Numerator.Out("Part 2 (expression): ").ShouldBe(3219579395609);
+
+        // Alternative solution. Binary search correct answer
+        monkeys["root"] = root with { Op = "-" };
         var hmnValue = 0L;
         var step = 1000000000000000;
         while (step > 0)
         {
-            var rootValue = ComputeValue(root.Name, monkeys, new Dictionary<string, long> { { "humn", hmnValue }, });
-            if (rootValue > 0)
+            var rootValue = ComputeValue(root.Name, monkeys, hmnValue);
+            if (rootValue.Numerator > 0)
                 hmnValue += step;
             else
                 hmnValue -= step;
@@ -37,7 +44,7 @@ public class Day21
         }
         hmnValue++;
         hmnValue
-            .Out("Part 2: ").ShouldBe(3219579395609);
+            .Out("Part 2 (bin search): ").ShouldBe(3219579395609);
     }
 
     private void PrintGraphViz(Monkey[] ms)
@@ -54,24 +61,29 @@ public class Day21
         File.WriteAllText("Day21.gv", res.ToString());
     }
 
-    private long ComputeValue(string name, IReadOnlyDictionary<string, Monkey> monkeys, Dictionary<string, long>? monkeyValues = null)
+    // h * humn + f
+    private (Rat HumanFactor, Rat Free) GetHumanExpression(string name, IReadOnlyDictionary<string, Monkey> monkeys)
     {
-        monkeyValues ??= new Dictionary<string, long>();
-        if (monkeyValues.TryGetValue(name, out var v))
-            return v;
+        if (name == "humn") 
+            return (1, 0);
         var m = monkeys[name];
         if (m.Op == null)
-            return monkeyValues[name] = long.Parse(m.A);
-        var a = ComputeValue(m.A, monkeys, monkeyValues);
-        var b = ComputeValue(m.B!, monkeys, monkeyValues);
-        var c = m.Op switch
+            return (0,long.Parse(m.A));
+        var (leftH, leftF) = GetHumanExpression(m.A, monkeys);
+        var (rightH, rightF) = GetHumanExpression(m.B!, monkeys);
+        return m.Op switch
         {
-            "-" => a - b,
-            "+" => a + b,
-            "*" => a * b,
-            "/" => a / b,
-            _ => throw new Exception(m.Op)
+            "+" => (leftH + rightH, leftF + rightF),
+            "-" => (leftH - rightH, leftF - rightF),
+            "*" when leftH*rightH == 0 => (leftH * rightF + leftF * rightH, leftF * rightF),
+            "/" when rightH == 0 => (leftH / rightF, leftF / rightF),
+            _ => throw new Exception(m.ToString())
         };
-        return monkeyValues[name] = c;
+    }
+
+    private Rat ComputeValue(string name, IReadOnlyDictionary<string, Monkey> monkeys, long? humanValue = null)
+    {
+        var (h, f) = GetHumanExpression(name, monkeys);
+        return (humanValue ?? long.Parse(monkeys["humn"].A)) * h + f;
     }
 }
