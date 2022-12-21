0ce9e7583a67abfe100e29629f5e7d2be57eb7e4
Pavel Egorov <xoposhiy@mail.ru>
2022-12-21 11:57:38 +0400

    Refactoring

--- 2022/solutions/Day21.cs
+++ 2022/solutions/Day21.cs
@@ -1,4 +1,5 @@
-﻿using Shouldly;
+﻿using System.Text;
+using Shouldly;
 
 // https://adventofcode.com/2022/day/21
 public class Day21
@@ -8,6 +9,7 @@ public class Day21
     // Для каждой обезьяны дано либо её значение, либо формула, как вычислить ее значение, по значениям других обезьян.
     public void Solve([Separator(": ")] Monkey[] ms)
     {
+        PrintGraphViz(ms);
         // Part1
         // Вычислить значение обезьяны по имени root
         var monkeys = ms.ToDictionary(m => m.Name);
@@ -19,29 +21,42 @@ public class Day21
         // Обезьяна root теперь сравнивает два своих аргумента на равенство.
         // А обезьяна humn − это вы. Какое значение должно быть у вас, чтобы у root получилось равенство?
         var root = monkeys["root"];
-        var aValue = ComputeValue(root.B!, monkeys)
-            .Out("root.A should be: ");
+        var res = new Dictionary<string, long>();
+        ComputeValue(root.A, monkeys, res);
+        var freeBranch = res.ContainsKey("humn") ? root.B! : root.A;
+        var humanBranch = root.A == freeBranch ? root.B! : root.A;
+        var freeBranchValue = ComputeValue(freeBranch, monkeys, res).Out("v: ");
 
+        // Binary search correct answer
         var hmnValue = 0L;
         var step = 1000000000000000;
         while (step > 0)
         {
-            var actualA = ComputeValue(root.A, monkeys, new Dictionary<string, long> { { "humn", hmnValue }, });
-            if (actualA == aValue)
-            {
-                //Console.WriteLine(hmnValue + " " + res[root.A] + " =? " + aValue + " " + (res[root.A] - aValue));
-                hmnValue--;
-                continue;
-            }
-            if (actualA > aValue)
+            var actualHumanBranchValue = ComputeValue(humanBranch, monkeys, new Dictionary<string, long> { { "humn", hmnValue }, });
+            if (actualHumanBranchValue > freeBranchValue)
                 hmnValue += step;
             else
                 hmnValue -= step;
             step /= 2;
         }
+        hmnValue++;
 
         hmnValue
             .Out("Part 2: ").ShouldBe(3219579395609);
+    }
+
+    private void PrintGraphViz(Monkey[] ms)
+    {
+        var res = new StringBuilder("digraph Day21 {\n");
+        res.AppendLine($" root[shape=box, color=red, style=filled];");
+        res.AppendLine($" humn[shape=box, color=red, style=filled];");
+        foreach (var monkey in ms.Where(m => m.Op != null))
+        {
+            res.AppendLine($"  {monkey.Name} -> {monkey.A};");
+            res.AppendLine($"  {monkey.Name} -> {monkey.B};");
+        }
+        res.AppendLine("}");
+        File.WriteAllText("Day21.gv", res.ToString());
     }
 
     private long ComputeValue(string name, IReadOnlyDictionary<string, Monkey> monkeys, Dictionary<string, long>? monkeyValues = null)
