3d6c8bd960187fdfd82d30463b24c72078dccb5e
Pavel Egorov <xoposhiy@mail.ru>
2023-12-10 18:25:45 +0400

    Refactor solution Day10-PipeMaze.cs

--- 2023/solutions/Day10-PipeMaze.cs
+++ 2023/solutions/Day10-PipeMaze.cs
@@ -4,114 +4,64 @@ public class Day10
 {
     public void Solve(char[][] map)
     {
-        /*
-| is a vertical pipe connecting north and south.
-- is a horizontal pipe connecting east and west.
-L is a 90-degree bend connecting north and east.
-J is a 90-degree bend connecting north and west.
-7 is a 90-degree bend connecting south and west.
-F is a 90-degree bend connecting south and east.
-. is ground; there is no pipe in this tile.
-S is the starting position of the animal; there is a pipe on this tile, but your sketch doesn't show what shape the pipe has.
-         */
-        IEnumerable<V> GetNext(PathItem<V> pos)
-        {
-            if (pos.Prev == null)
-            {
-                if (map.Get(pos.State + V.Up).IsOneOf('|', 'F', '7'))
-                    yield return pos.State + V.Up;
-                if (map.Get(pos.State + V.Down).IsOneOf('|', 'L', 'J'))
-                    yield return pos.State + V.Down;
-                if (map.Get(pos.State + V.Left).IsOneOf('-', 'L', 'F'))
-                    yield return pos.State + V.Left;
-                if (map.Get(pos.State + V.Right).IsOneOf('-', 'J', '7'))
-                    yield return pos.State + V.Right;
-            }
-            else
-            {
-                var sym = map.Get(pos.State);
-                var d = pos.State - pos.Prev.State;
-                if (sym is '|' or '-') yield return pos.State + d;
-                if (sym is '7' && d == V.Up) yield return pos.State + V.Left;
-                if (sym is '7' && d == V.Right) yield return pos.State + V.Down;
-                
-                if (sym is 'L' && d == V.Down) yield return pos.State + V.Right;
-                if (sym is 'L' && d == V.Left) yield return pos.State + V.Up;
-
-                if (sym is 'J' && d == V.Down) yield return pos.State + V.Left;
-                if (sym is 'J' && d == V.Right) yield return pos.State + V.Up;
-
-                if (sym is 'F' && d == V.Up) yield return pos.State + V.Right;
-                if (sym is 'F' && d == V.Left) yield return pos.State + V.Down;
-
-            }
-        }
-
-        var start = map.GetPosition('S');
-        var path = GraphSearch.Bfs(GetNext, int.MaxValue, start);
-        path.Max(p => p.Len).Part1();
-
-        var left = new HashSet<V>();
-        var right = new HashSet<V>();
-
-        var me = new PathItem<V>(start, null, 0);
-        while (true)
-        {
-            //L-
-            var nextPos = GetNext(me).First();
-            var d = nextPos - me.State;
-            right.Add(me.State + d.RotateCW());
-            left.Add(me.State + d.RotateCW());
-            if (me.Prev != null)
-            {
-                var prevDir = me.State - me.Prev!.State;
-                var other = me.State - d;
-                if (prevDir.RotateCCW() == d)
-                    right.Add(other);
-                if (prevDir.RotateCW() == d)
-                    left.Add(other);
-            }
-
-            if (nextPos == start)
-            {
-                break;
-            }
-            me = new PathItem<V>(nextPos, me, me.Len + 1);
-        }
-        // var used = path.Select(p => p.State).ToHashSet();
-        left.ExceptWith(path.Select(p => p.State));
-        right.ExceptWith(path.Select(p => p.State));
-        left.Count().Out("left: ");
-        right.Count.Out("right: ");
+        var startPos = map.GetPosition('S');
+        var startSym = GetSymbol(GetNext(startPos).Select(n => n - startPos).ToArray()).Out("start symbol: ");
+        map.Set(startPos, startSym);
+        var loop = GraphSearch.Dfs(p => GetNext(p.State), startPos)
+            .Select(p => p.State).ToHashSet();
+        (loop.Count/2).Part1();
         
-        var used = path.Select(p => p.State).ToHashSet();
-        var startPositions = right;
-        startPositions.Count.Out("count: ");
-        
-        var sum = 0;
-        var res = new List<V>();
-        foreach (V pos in startPositions)
+        var innerCellsCount = 0;
+        for (int y = 0; y < map.Height(); y++)
         {
-            if (used.Contains(pos)) continue;
-            var paths2 = GraphSearch.Bfs(p => 
-                p.State.Area4().Where(n => !used.Contains(n) && n.InRange(map)), int.MaxValue, pos);
-            foreach (var item in paths2)
+            // Consider the ray from (x, y) to (0, y).
+            // Count the doubled number of crosses of this ray with loop border.
+            var doubledCrosses = map.Width()*10;
+            for (int x = 0; x < map.Width(); x++)
             {
-                used.Add(item.State);
-            }
-            if (!paths2.Any(p => p.State.X.IsOneOf(0, map[0].Length - 1) || p.State.Y.IsOneOf(0, map.Length - 1)))
-            {
-                sum += paths2.Count;
-                foreach (var item in paths2)
+                if (!loop.Contains(new V(x, y)))
                 {
-                    res.Add(item.State);
+                    if (doubledCrosses / 2 % 2 == 1) innerCellsCount++;
+                }
+                else
+                {
+                    var c = map[y][x];
+                    if (c == '|') doubledCrosses += 2;
+                    if (c == 'L') doubledCrosses += 1;
+                    if (c == 'J') doubledCrosses -= 1;
+                    if (c == '7') doubledCrosses += 1;
+                    if (c == 'F') doubledCrosses -= 1;
                 }
             }
-            
         }
-        
-        sum.Part2();
+        innerCellsCount.Part2();
         return;
-    }
 
+        char GetSymbol(V[] exitDirs)
+        {
+            if (exitDirs.SetEquals(V.UpDown)) return '|';
+            if (exitDirs.SetEquals(V.LeftRight)) return '-';
+            if (exitDirs.SetEquals(V.RightUp)) return 'L';
+            if (exitDirs.SetEquals(V.LeftUp)) return 'J';
+            if (exitDirs.SetEquals(V.LeftDown)) return '7';
+            if (exitDirs.SetEquals(V.RightDown)) return 'F';
+            throw new Exception(exitDirs.Format());
+        }
+
+        IEnumerable<V> PossibleDirs(V pos) =>
+            map.Get(pos) switch
+            {
+                '|' => V.UpDown,
+                '-' => V.LeftRight,
+                'L' => V.RightUp,
+                'J' => V.LeftUp,
+                '7' => V.LeftDown,
+                'F' => V.RightDown,
+                'S' => V.Directions4,
+                _ => Enumerable.Empty<V>(),
+            };
+
+        IEnumerable<V> GetNext(V pos) => 
+            PossibleDirs(pos).Where(d => PossibleDirs(pos + d).Contains(-d)).Select(d => pos + d);
+    }
 }
